2829e4d7cd5d21c434890e1587c1833462966a36
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-06-22 16:26:28 +0300

    CSS - Transition

--- quote-machine/main.js
+++ quote-machine/main.js
@@ -2,12 +2,14 @@ new Vue({
     el: "#app",
     data: {
         quote_array: quote_array,
-        current_quote: quote_array[0]
+        current_quote: quote_array[0],
+        show: true
     },
     methods: {
         newQuote: function() {
             const num = Math.floor(Math.random() * quote_array.length)
             this.current_quote = quote_array[num];
+            this.show = this.show
         }
     },
 });
